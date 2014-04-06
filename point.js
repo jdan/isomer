f19e4e0fa7bd7cfe7c7c06e4ecfbf0fd4d4229b9
Jordan Scales <scalesjordan@gmail.com>
2014-04-05 23:12:09 -0400

    Point#translate and Point#rotateZ

--- point.js
+++ point.js
@@ -3,3 +3,24 @@ function Point(x, y, z) {
   this.y = y;
   this.z = z || 0;
 }
+
+Point.prototype.translate = function (dx, dy, dz) {
+  return new Point(
+    this.x + dx,
+    this.y + dy,
+    this.z + dz);
+};
+
+/**
+ * Rotate about origin on the Z axis
+ */
+Point.prototype.rotateZ = function (origin, angle) {
+  var p = this.translate(-origin.x, -origin.y, -origin.z);
+
+  var x = p.x * Math.cos(angle) - p.y * Math.sin(angle);
+  var y = p.x * Math.sin(angle) + p.y * Math.cos(angle);
+  p.x = x;
+  p.y = y;
+
+  return p.translate(origin.x, origin.y, origin.z);
+};
