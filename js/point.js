89c0c5142a8a2ade86262eb3172cb6381ce7169b
Jordan Scales <scalesjordan@gmail.com>
2014-04-26 19:40:29 -0400

    allow Point to be called without the new keyword

--- js/point.js
+++ js/point.js
@@ -1,9 +1,13 @@
 (function (exports) {
 
   function Point(x, y, z) {
-    this.x = x || 0;
-    this.y = y || 0;
-    this.z = z || 0;
+    if (this instanceof Point) {
+      this.x = x || 0;
+      this.y = y || 0;
+      this.z = z || 0;
+    } else {
+      return new Point(x, y, z);
+    }
   }
 
 
