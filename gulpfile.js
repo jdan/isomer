c1f89daed5023a9a1411d7efa190c543211abb01
Hunter Trujillo <cryptoquick@gmail.com>
2014-04-30 19:07:19 -0600

    Add support for Node.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var insert = require('gulp-insert');
 
 /* Main gulp task to minify and concat assets */
 gulp.task('build', function () {
@@ -14,6 +15,13 @@ gulp.task('build', function () {
 gulp.task('concat', function () {
   gulp.src(['./js/isomer.js', './js/!(isomer)*.js'])
     .pipe(concat('isomer.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('node', function () {
+  gulp.src(['./js/isomer.js', './js/!(isomer)*.js'])
+    .pipe(concat('isomer.js'))
+    .pipe(insert.append('\nmodule.exports = Isomer;'))
     .pipe(gulp.dest('./build'));
 });
 
