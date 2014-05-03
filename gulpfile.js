3d3669b8d44e2ce60b505b0e43ed4923fb3b6659
Jordan Scales <scalesjordan@gmail.com>
2014-05-03 16:17:28 -0400

    converted build process to use browserify; changed entry point in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,13 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var insert = require('gulp-insert');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 /* Main gulp task to minify and concat assets */
 gulp.task('build', function () {
-  gulp.src(['./js/isomer.js', './js/!(isomer)*.js'])
-    .pipe(uglify())
-    .pipe(concat('isomer.min.js'))
-    .pipe(gulp.dest('./build'));
-});
+  var bundleStream = browserify('./index.js').bundle();
 
-/* Task for testing purposes - concat without minifying */
-gulp.task('concat', function () {
-  gulp.src(['./js/isomer.js', './js/!(isomer)*.js'])
-    .pipe(concat('isomer.js'))
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('node', function () {
-  gulp.src(['./js/isomer.js', './js/!(isomer)*.js'])
-    .pipe(concat('isomer.js'))
-    .pipe(insert.append('\nmodule.exports = Isomer;'))
+  bundleStream
+    .pipe(source('isomer.js'))
     .pipe(gulp.dest('./build'));
 });
 
