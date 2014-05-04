125b5489d78b2edc9fe808d7cba9d731a3f21b87
Jordan Scales <scalesjordan@gmail.com>
2014-05-04 14:16:24 -0400

    added copyright banner to build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,35 @@
+var browserify = require('browserify');
+var fs = require('fs');
 var gulp = require('gulp');
+var buffer = require('gulp-buffer');
+var header = require('gulp-header');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var strftime = require('strftime');
+
+var version = require('./package.json').version;
+var date = strftime('%F');
 
 /* Main gulp task to minify and concat assets */
 gulp.task('build', function () {
   var bundleStream = browserify('./index.js').bundle();
+  var banner = fs.readFileSync('./js/banner/copyright.js');
 
   bundleStream
     .pipe(source('isomer.js'))
+    .pipe(buffer())
+    .pipe(header(banner, { date: date, version: version }))
     .pipe(gulp.dest('./build'));
 });
 
 /* Task to create a release by minifying the build */
 gulp.task('release', ['build'], function () {
+  var banner = fs.readFileSync('./js/banner/copyright.min.js');
+
   gulp.src('./build/isomer.js')
     .pipe(uglify())
+    .pipe(header(banner, { version: version }))
     .pipe(rename('isomer.min.js'))
     .pipe(gulp.dest('./build'));
 });
---
+++ js/banner/copyright.js
@@ -0,0 +1,10 @@
+/*!
+ * Isomer v<%= version %>
+ * http://jdan.github.io/isomer/
+ *
+ * Copyright 2014 Jordan Scales
+ * Released under the MIT license
+ * http://jdan.github.io/isomer/license.txt
+ *
+ * Date: <%= date %>
+ */
