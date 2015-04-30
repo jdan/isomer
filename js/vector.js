8ab1b6d77cdc127f185055f4267c661af972fedf
Dane Pilcher <dane.pilcher@icloud.com>
2015-04-30 17:13:52 -0400

    add toPoint

--- js/vector.js
+++ js/vector.js
@@ -23,6 +23,13 @@ Vector.dotProduct = function(v1, v2) {
   return v1.i * v2.i + v1.j * v2.j + v1.k * v2.k;
 };
 
+/**
+ * turn a vector into a point
+ */
+Vector.toPoint = function(vec) {
+    return new Point(vec.i, vec.j, vec.k);
+}
+
 Vector.prototype.magnitude = function() {
   return Math.sqrt(this.i * this.i + this.j * this.j + this.k * this.k);
 };
