6bb8a51b3bfbdd1957332275f7819a0d9a1b7079
Dane Pilcher <dane.pilcher@icloud.com>
2015-05-06 18:45:31 -0400

    test for radius

--- test/test.js
+++ test/test.js
@@ -232,9 +232,10 @@ TestSuite['vectorToPoint'] = function() {
 
 TestSuite['draw sphere'] = function() {
     iso.add(Shape.Prism(Point.ORIGIN, 4, 4, 1));
-    //iso.add(Shape.Sphere(new Point(2.5,1.5,1.5)), new Color(0, 0, 195));
     iso.add(Shape.Prism(new Point(0,0,0)), new Color(195, 195, 0));
-    iso.add(Shape.Sphere(new Point(0.5,0.5,0.5)), new Color(195, 0, 0));
+    iso.add(Shape.Sphere(new Point(0.5,0.5,1.5)), new Color(195, 0, 0));
+    iso.add(Shape.Prism(new Point(2, 2, 1), 2, 2, 1), new Color(0, 195, 0));
+    iso.add(Shape.Sphere(new Point(3,3,3), 1), new Color(0, 195, 195));
 };
 
 
