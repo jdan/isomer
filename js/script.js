4a125e4bf05457b26f7186c3a5f56c03a7ac8ce3
Jordan Scales <scalesjordan@gmail.com>
2015-06-20 10:20:41 -0400

    added requestAnimationFrame shim

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ var Path = Isomer.Path;
 var Shape = Isomer.Shape;
 var Color = Isomer.Color;
 
+// requestAnimationFrame shim
+// http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimationFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 function Stairs(origin) {
   var STEP_COUNT = 10;
 
